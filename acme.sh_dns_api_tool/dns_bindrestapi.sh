63c1bd2705b5932347e317df4b49d24653ea6fb1
Jay Tuckey <jay.tuckey@cdu.edu.au>
2020-10-08 09:08:48 +0930

    More details in acme.sh dns tool

--- acme.sh_dns_api_tool/dns_bindrestapi.sh
+++ acme.sh_dns_api_tool/dns_bindrestapi.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env sh
 
-#
-# bindrestapi_key="abc123"
-#
+# This shell script lets you issue letsencrypt certificates with this bind API
+# see https://acme.sh
+# export bindrestapi_key="abc123"
+# export bindrestapi_url="https://dyndns1.example.org"
 
-bindrestapiurl="https://dyndnsdev1.cdu.edu.au"
-
+# Testing notes - command used to test:
+# ./acme.sh --insecure --issue --staging --debug 2 --domain test.example.org --dns dns_bindrestapi | tee debug_run.log
 # Usage: add _acme-challenge.host.example.org "xxzzyy"
 
 dns_bindrestapi_add() {
   fulldomain=$1
   txtvalue=$2
-
-  _info "GrepForMe! - add"
 
   if [ "$bindrestapi_key" ]; then
     _saveaccountconf_mutable bindrestapi_key "$bindrestapi_key"
@@ -21,8 +20,12 @@ dns_bindrestapi_add() {
     return 1
   fi
 
-  _debug "First get root zone"
-  # pass - not sure what to do here exactly
+  if [ "$bindrestapi_url" ]; then
+    _saveaccountconf_mutable bindrestapi_url "$bindrestapi_url"
+  else
+    _err "You didn't specify a bindrestapi_url - export bindrestapi_url=\"https://dyndns1.example.org\""
+    return 1
+  fi
 
   _info "Adding record $fulldomain"
   export _H1="X-Api-Key: $bindrestapi_key"
@@ -37,8 +40,6 @@ dns_bindrestapi_rm() {
   fulldomain=$1
   txtvalue=$2
 
-  _info "GrepForMe! - rm"
-
   if [ "$bindrestapi_key" ]; then
     _saveaccountconf_mutable bindrestapi_key "$bindrestapi_key"
   else
@@ -46,8 +47,12 @@ dns_bindrestapi_rm() {
     return 1
   fi
 
-  _debug "First get root zone"
-  # pass - not sure what to do here exactly
+  if [ "$bindrestapi_url" ]; then
+    _saveaccountconf_mutable bindrestapi_url "$bindrestapi_url"
+  else
+    _err "You didn't specify a bindrestapi_url - export bindrestapi_url=\"https://dyndns1.example.org\""
+    return 1
+  fi
 
   _info "deleting record $fulldomain"
   export _H1="X-Api-Key: $bindrestapi_key"
