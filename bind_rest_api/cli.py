becf6554fc7074cd1ba61ddded614e148a7bdb9f
Alex Kelly <kellya@arachnitech.com>
2021-09-14 13:06:52 -0400

    test: add coverage for all cli calls
    feat: add a dry-run option to do nothing for testing

--- bind_rest_api/cli.py
+++ bind_rest_api/cli.py
@@ -12,18 +12,25 @@ from bind_rest_api.api.constants import VERSION
     "--port", "-P", default=8000, help="TCP port to bind to for the api service"
 )
 @click.option("--workers", "-w", default=3, help="Number of workers to deploy")
+@click.option("--dry-run", "-n", is_flag=True, help="Do not actually run - for testing")
 @click.version_option(version=VERSION)
-def main(host, port, workers):
+def main(host, port, workers, dry_run):
     """main logic for the cli"""
-    uvicorn.run(
-        "bind_rest_api.api.api:app",
-        host=host,
-        port=port,
-        reload=True,
-        debug=True,
-        workers=workers,
-    )
+    if not dry_run:  # pragma: no cover
+        uvicorn.run(
+            "bind_rest_api.api.api:app",
+            host=host,
+            port=port,
+            reload=True,
+            debug=True,
+            workers=workers,
+        )
+    else:
+        print("would run with the following options:")
+        print(f"host: {host}")
+        print(f"port: {port}")
+        print(f"workers: {workers}")
 
 
 if __name__ == "__main__":
-    main()  # pylint: disable=no-value-for-parameter
+    main()  # pylint: disable=no-value-for-parameter #pragma: no cover
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ runner = CliRunner()
 
 def test_main():
     """validate main runs with successful exit"""
-    response = runner.invoke(cli_main)
+    response = runner.invoke(cli_main, ["--dry-run"])
     assert response.exit_code == 0
 
 
@@ -23,3 +23,17 @@ def test_help():
     """verify --help outputs help information"""
     response = runner.invoke(cli_main, ["--help"])
     assert response.exit_code == 0
+
+
+def test_host():
+    """verify host specified is applied to command"""
+    response = runner.invoke(cli_main, ["--dry-run", "--host", "1.2.3.4"])
+    assert response.exit_code == 0
+    assert "1.2.3.4" in response.output
+
+
+def test_port():
+    """verify port specified is applied to command"""
+    response = runner.invoke(cli_main, ["--dry-run", "--port", "9999"])
+    assert response.exit_code == 0
+    assert "9999" in response.output
